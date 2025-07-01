4ae049ede0ce0056ebbc84e6b12fd6dc9bcd0802
Ivan Ryzhikov <107796289+coollappsus@users.noreply.github.com>
2025-07-01 13:25:49 +0300

    init

---
+++ src/main/java/com/example/gorzdrav_spb_bot/GorzdravSpbBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.gorzdrav_spb_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GorzdravSpbBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GorzdravSpbBotApplication.class, args);
+    }
+
+}
