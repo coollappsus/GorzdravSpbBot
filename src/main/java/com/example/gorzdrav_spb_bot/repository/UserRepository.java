68f070b6a57109ec94057995375dfaa251079895
Ivan Ryzhikov <107796289+coollappsus@users.noreply.github.com>
2025-08-13 16:18:11 +0300

    Оповещение о новых пользователях

--- src/main/java/com/example/gorzdrav_spb_bot/repository/UserRepository.java
+++ src/main/java/com/example/gorzdrav_spb_bot/repository/UserRepository.java
@@ -2,11 +2,17 @@ package com.example.gorzdrav_spb_bot.repository;
 
 import com.example.gorzdrav_spb_bot.model.User;
 import lombok.NonNull;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface UserRepository extends CrudRepository<User, Long> {
 
     boolean existsByUserId(Long userId);
 
     User findUserByUserId(@NonNull Long id);
+
+    @Query(value = "SELECT * FROM public.get_user_delta()", nativeQuery = true)
+    List<User> getNewUsers();
 }
---
+++ src/main/java/com/example/gorzdrav_spb_bot/service/UserChecker.java
@@ -0,0 +1,37 @@
+package com.example.gorzdrav_spb_bot.service;
+
+import com.example.gorzdrav_spb_bot.model.User;
+import com.example.gorzdrav_spb_bot.repository.UserRepository;
+import com.example.gorzdrav_spb_bot.service.telegram.TelegramAsyncMessageSender;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@AllArgsConstructor
+@Slf4j
+public class UserChecker {
+
+    private final static Long ADMIN_ID = 906044021L;
+
+    private final UserRepository userRepository;
+    private final TelegramAsyncMessageSender telegramAsyncMessageSender;
+
+    @Scheduled(fixedDelay = 3600000) //Раз в 1 час
+    public void checkNewUser() {
+        List<User> newUsers = userRepository.getNewUsers();
+
+        if (newUsers.isEmpty()) return;
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(newUsers.size()).append("New users found\n");
+        for (User user : newUsers) {
+            sb.append(user.getUserName()).append("\n");
+        }
+
+        telegramAsyncMessageSender.sendMessageToUser(ADMIN_ID, sb.toString());
+    }
+}
