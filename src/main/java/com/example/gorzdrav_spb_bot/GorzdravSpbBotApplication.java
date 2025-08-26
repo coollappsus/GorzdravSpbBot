670b2df5cfefa82523540d8eea984e3e1cd6820f
Ivan Ryzhikov <107796289+coollappsus@users.noreply.github.com>
2025-08-26 11:57:01 +0300

    Правки ИБ

--- src/main/java/com/example/gorzdrav_spb_bot/GorzdravSpbBotApplication.java
+++ src/main/java/com/example/gorzdrav_spb_bot/GorzdravSpbBotApplication.java
@@ -1,40 +1,15 @@
 package com.example.gorzdrav_spb_bot;
 
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
-import com.ulisesbocchio.jasyptspringboot.encryptor.SimpleAsymmetricConfig;
-import com.ulisesbocchio.jasyptspringboot.encryptor.SimpleAsymmetricStringEncryptor;
-import com.ulisesbocchio.jasyptspringboot.encryptor.SimplePBEStringEncryptor;
-import org.jasypt.encryption.StringEncryptor;
-import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
-import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
-import org.jasypt.util.text.BasicTextEncryptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-
-import static org.jasypt.properties.PropertyValueEncryptionUtils.encrypt;
 
 @SpringBootApplication
-@EnableEncryptableProperties
 @EnableScheduling
 public class GorzdravSpbBotApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GorzdravSpbBotApplication.class, args);
-
-//        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
-//        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-//        config.setPassword("token");
-//        // дальше дефолтные параметры
-//        config.setAlgorithm("PBEWithMD5AndDES");
-//        config.setKeyObtentionIterations("1000");
-//        config.setPoolSize("1");
-//        config.setProviderName("SunJCE");
-//        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
-//        config.setStringOutputType("base64");
-//        encryptor.setConfig(config);
-//        System.out.println("Encrypted Value: " + encryptor.encrypt("7633491556:AAEmwWhTjEu63XGgfrMpjrZXVKHsy_WxJ_M"));
     }
 
 }
