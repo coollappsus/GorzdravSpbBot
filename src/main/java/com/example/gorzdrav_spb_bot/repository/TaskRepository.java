1d0a80252896236e69877d9c55ee48b49815b67e
Ivan Ryzhikov <107796289+coollappsus@users.noreply.github.com>
2025-10-07 20:23:52 +0300

    Добавление логики прибивания неактуальных тасок.

--- src/main/java/com/example/gorzdrav_spb_bot/repository/TaskRepository.java
+++ src/main/java/com/example/gorzdrav_spb_bot/repository/TaskRepository.java
@@ -3,9 +3,14 @@ package com.example.gorzdrav_spb_bot.repository;
 import com.example.gorzdrav_spb_bot.model.Task;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Date;
 import java.util.List;
 
 public interface TaskRepository extends CrudRepository<Task, Long> {
 
     List<Task> findByCompleteStatusAndActiveStatus(Boolean completeStatus, Boolean activeStatus);
+
+    List<Task> findByPreferenceDateBeforeAndCompleteStatusAndActiveStatus(Date preferenceDateBefore,
+                                                                          Boolean completeStatus,
+                                                                          Boolean activeStatus);
 }
---
+++ src/main/java/com/example/gorzdrav_spb_bot/service/OldTasksChecker.java
@@ -0,0 +1,37 @@
+package com.example.gorzdrav_spb_bot.service;
+
+import com.example.gorzdrav_spb_bot.model.Task;
+import com.example.gorzdrav_spb_bot.repository.TaskRepository;
+import com.example.gorzdrav_spb_bot.service.telegram.TelegramAsyncMessageSender;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
+
+@Component
+@AllArgsConstructor
+@Slf4j
+public class OldTasksChecker {
+
+    private final static Long ADMIN_ID = 906044021L;
+    private final static String MESSAGE_TEXT = "OLD TASKS SUCCEEDED REMOVED. COUNT - %s";
+
+    private final TaskRepository taskRepository;
+    private final TelegramAsyncMessageSender telegramAsyncMessageSender;
+
+    @Scheduled(cron = "0 5 0 * * *", zone = "Europe/Moscow")
+    public void checkAndRemoveOldTasks() {
+        log.info("Checking old tasks");
+        List<Task> oldTasks = taskRepository.findByPreferenceDateBeforeAndCompleteStatusAndActiveStatus(new Date(),
+                false, true);
+        for (Task task : oldTasks) {
+            task.setActiveStatus(false);
+        }
+
+        log.info("Saving old tasks and notify admin");
+        telegramAsyncMessageSender.sendMessageToUser(ADMIN_ID, MESSAGE_TEXT.formatted(oldTasks.size()));
+    }
+}
