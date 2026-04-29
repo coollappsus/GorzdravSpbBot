396a81fbb10091cab79e1410fabe13e5e126d772
Ivan Ryzhikov <107796289+coollappsus@users.noreply.github.com>
2026-04-29 11:56:16 +0300

    -Обработка Rate limit reached

--- src/main/java/com/example/gorzdrav_spb_bot/GorzdravSpbBotApplication.java
+++ src/main/java/com/example/gorzdrav_spb_bot/GorzdravSpbBotApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.concurrent.TimeUnit;
+
 @SpringBootApplication
 @EnableScheduling
 @Slf4j
@@ -19,15 +21,22 @@ public class GorzdravSpbBotApplication {
 
         // Запускаем бесконечный цикл "живучести"
         while (true) {
+            long countDaysSleep = 0;
             try {
                 log.info("Запуск LongPoll сессии...");
                 app.startPolling();
             } catch (VkApiException e) {
                 // Тот самый таймаут или любая другая сетевая ошибка упадет сюда
-                log.error("Сетевая ошибка VK (возможно таймаут). Пробую переподключиться через 10 секунд...", e);
                 try {
-                    // Обязательно делаем паузу, чтобы не долбить сервер в случае жесткого сбоя
-                    Thread.sleep(100000);
+                    if (e.getMessage().contains("Rate limit reached")) {
+                        countDaysSleep++;
+                        log.error("Поймали лимит запросов к серверу. Придется ждать долго. Спим столько суток - " + countDaysSleep);
+                        TimeUnit.DAYS.sleep(countDaysSleep);
+                    } else {
+                        log.error("Сетевая ошибка VK (возможно таймаут). Пробую переподключиться через 3 минуты...", e);
+                        // Обязательно делаем паузу, чтобы не долбить сервер в случае жесткого сбоя
+                        TimeUnit.MINUTES.sleep(3);
+                    }
                 } catch (InterruptedException ie) {
                     Thread.currentThread().interrupt();
                     break; // Если поток прерван, выходим
